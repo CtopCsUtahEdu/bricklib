ef30eeb08a56fbd509ce965f699573448ebbd882
Samantha Hirsch <sam.hirsch66@gmail.com>
2021-06-22 12:40:38 -0400

    added initial header file for hip stencil test cases

---
+++ stencils/stencils_hip.h
@@ -0,0 +1,32 @@
+//
+// Created by Samantha Hirsch on 6/21/21
+//
+
+#ifndef BRICK_STENCILS_HIP_H
+#define BRICK_STENCILS_HIP_H
+
+#define HIP_ITER 100
+
+template<typename T>
+double hiptime_func(T func) {
+    func();
+    hipEvent_t start_event, stop_event;
+    hipDeviceSynchronize();
+    hipEventCreate(&start_event);
+    hipEventCreate(&stop_event);
+    hipEventRecord(start_event);
+    for (int i = 0; i < HIP_ITER; i++) {
+        func();
+    }
+    hipEventRecord(stop_event);
+    hipEventSynchronize(stop_event);
+    hipEventSynchronize(start_event);
+    float elapsed;
+    hipEventElapsedTime(&elapsed, start_event, stop_event);
+    const unsigned ms_per_s = 1000;
+    return elapsed / HIP_ITER / ms_per_s;
+}
+
+void d3pt7cu();
+
+#endif // fully defined
