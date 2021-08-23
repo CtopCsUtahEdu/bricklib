94cc9096f8ff459798c80ecdb82e51d8fc1edfae
Tuowen Zhao <ztuowen@gmail.com>
2021-08-23 12:08:10 -0400

    SVE fixes

--- codegen/st/codegen/backend/asimd.py
+++ codegen/st/codegen/backend/asimd.py
@@ -36,7 +36,7 @@ class BackendASIMD(BackendAVX512):
 class BackendSVE(BackendAVX512):
     def __init__(self, length):
         super().__init__()
-        self.VECLEN = length/32
+        self.VECLEN = length//32
 
     def checkConfig(self):
         super().checkConfig()
@@ -48,18 +48,18 @@ class BackendSVE(BackendAVX512):
         block.append("svuint32_t {};".format(name))
 
     def store_vecbuf(self, vecbuf_name, reg_name, block: CodeBlock):
-        block.append("svst1_u32(pg, (uint32_t *) & {}[0], {});".format(reg_name, vecbuf_name))
+        block.append("svst1_u32(svptrue_b32(), (uint32_t *) & {}[0], {});".format(reg_name, vecbuf_name))
 
     def read_aligned(self, grid: Grid, offset, name: str, block: CodeBlock, rel=None):
         block.append("// read {} -> {}".format(str(offset), name))
         if rel is not None:
             rel = [rel]
-        block.append("{} = svld1_u32(pg, (uint32_t *) & {});".format(
+        block.append("{} = svld1_u32(svptrue_b32(), (uint32_t *) & {});".format(
             name, self.layout.elem(grid, offset, rel)))
 
     def merge(self, rego, regl, regr, dim, shift, block: CodeBlock):
         block.append("// merge{} {} ,{}, {} -> {}".format(dim, regl, regr, shift, rego))
         if dim > 0:
-            raise RuntimeError("Cannot merge on dimension {} for ASIMD".format(dim))
-        block.append("{} = vext_u32({}, {}, {});".format(rego, regl, regr, shift * self.prec))
+            raise RuntimeError("Cannot merge on dimension {} for SVE".format(dim))
+        block.append("{} = svext_u32({}, {}, {});".format(rego, regl, regr, shift * self.prec))
 
